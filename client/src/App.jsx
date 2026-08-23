b94d8f6984254002b2bb08e418b25f6044d945ce
Riad Bhuiyan <riadbhuiyan2004@gmail.com>
2026-08-23 21:52:59 +0600

    fix Home page import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import {Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 
